188d5b74f3d18afac0c422613508462ec83a2a93
root <root@localhost>
2024-09-03 16:50:08 +0000

    US-1-BT-1--Develop APIs to handle job listing creation and retrieval. Employers should be able to specify job details and manage their listings effectively.

--- server/app/views.py
+++ server/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Job
+from .serializers import JobSerializer
 
-# Create your views here.
+class JobViewSet(viewsets.ViewSet):
+    def create(self, request):
+        serializer = JobSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save() 
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request, employer_id):
+        jobs = Job.objects.filter(employer_id=employer_id)
+        serializer = JobSerializer(jobs, many=True)
+        return Response(serializer.data)
